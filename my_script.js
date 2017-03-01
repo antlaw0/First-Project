0a4d54f5c41c07d54e3fe54e5391d82d8a79f956
AAA <aaa@aaa.com>
2017-02-28 21:13:30 -0600

    Sub-system controls-done, travel mechanic-done, added redraw function

--- my_script.js
+++ my_script.js
@@ -1,7 +1,10 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var selectionBoxX=0;
-var selectionBoxY=0;
+var infoArea=document.getElementById("info");
+
+var goButton = document.getElementById("goButton");
+goButton.addEventListener('click', travel);
+var selectedLocation=null;
 var mx=0;
 var my=0;
 
@@ -38,7 +41,7 @@ drawCircle(this.x, this.y, 5, color);
 
 //initialize all locations
 var Sol = new Location("Sol System", "Human home system. Home to human-kind. Breathable atmosphere.", canvas.width/2, canvas.height/2, "blue");
-var Alpha = new Location("Alpha Centauri", "Nearest star system to Earth. Consists of three stars, Alpha, Beta, and Proxima.", canvas.width/2+10, canvas.height/2+10, "red");
+var Alpha = new Location("Alpha Centauri", "Nearest star system to Earth. Consists of three stars, Alpha, Beta, and Proxima.", canvas.width/2+30, canvas.height/2+30, "red");
 
 var ship = function() {
   this.name = "Ship";
@@ -51,14 +54,13 @@ context.fillText("You are here", this.x+10, this.y);
 var ship = new ship();
 
 
-
+//set initial HTML of the info area
+infoArea.innerHTML=Sol.name+"<br>"+Sol.description;
 
 function showInfo()
 {
-var infoArea=document.getElementById("info");
 var clickBox=10;
 var l=null;
-context.clearRect(0,0,canvas.width,canvas.height);
 	
 for (var i=0; i<all_locations.length; i++)
 {
@@ -68,39 +70,54 @@ if (my > all_locations[i].y-clickBox && my < all_locations[i].y+clickBox)
 
 {
 l=all_locations[i];
-context.strokeStyle="yellow";
-context.strokeRect(l.x-10, l.y-10, 20, 20);
-
+selectedLocation=l;
 infoArea.innerHTML=l.name+
 "<br>"+
 l.description+"<br>Distance from current position: "
 +parseInt(getDistance(ship.x, ship.y, l.x, l.y))+" parsecs";
-}
 
-drawCircle(all_locations[i].x, all_locations[i].y, 10, all_locations[i].color);
-context.font = "12px Arial";
-context.fillStyle="white";
-context.fillText("You are here",ship.x+10, ship.y);
 
 }
-	
+
+
+}
+redrawCanvas();	
 }
 
 var shipHealth=100;
+var enginesScore=100;
+var sensorsScore=100;
+var weaponsScore=100;
+var shieldsScore =100;
+var weaponsModifiedElement=document.getElementById("weaponsModifiedRating");
+var enginesModifiedElement=document.getElementById("enginesModifiedRating");
+var shieldsModifiedElement=document.getElementById("shieldsModifiedRating");
+var sensorsModifiedElement=document.getElementById("sensorsModifiedRating");
+var energyDisplayArea=document.getElementById("displayEnergy");
+
 var displayShipHealthElement = document.getElementById("displayHealth");
 displayShipHealthElement.innerHTML= "Ship Health:  "+shipHealth;
-var enginesSlider=document.getElementById("engines");
-var weaponsSlider=document.getElementById("weapons");
-var shieldsSlider=document.getElementById("shields");
-var sensorsSlider=document.getElementById("sensors");
 var displayEnginesValue=document.getElementById("displayEnginesValue");
 var maxEnergy=1000;
 var energy= maxEnergy;
 document.getElementById("totalEnergy").innerHTML= "Total Energy to be consumed:  "+getTotalEnergyUse();
+var enginesPercent = 50;
+var weaponsPercent=50;
+var shieldsPercent=50;
+var sensorsPercent=50;
+
+enginesModifiedElement.innerHTML=enginesScore*(enginesPercent/100);
+weaponsModifiedElement.innerHTML=weaponsScore*(weaponsPercent/100);
+shieldsModifiedElement.innerHTML=shieldsScore*(shieldsPercent/100);
+sensorsModifiedElement.innerHTML=sensorsScore*(sensorsPercent/100);
+
+updateValues();
 
 function showEnginesValue(newValue)
 {
 	document.getElementById("enginesDisplayValue").innerHTML=newValue;
+	enginesPercent=newValue;
+	
 	updateValues();
 }
 
@@ -108,6 +125,7 @@ function showEnginesValue(newValue)
 function showWeaponsValue(newValue)
 {
 	document.getElementById("weaponsDisplayValue").innerHTML=newValue;
+	weaponsPercent=newValue;
 	updateValues();
 
 }
@@ -115,6 +133,7 @@ function showWeaponsValue(newValue)
 function showShieldsValue(newValue)
 {
 	document.getElementById("shieldsDisplayValue").innerHTML=newValue;
+	shieldsPercent=newValue;
 		updateValues();
 }
 
@@ -122,26 +141,42 @@ function showShieldsValue(newValue)
 function showSensorsValue(newValue)
 {
 	document.getElementById("sensorsDisplayValue").innerHTML=newValue;
+	sensorsPercent=newValue;
 		updateValues();
 }
 
-  var energyDisplayArea=document.getElementById("displayEnergy");
-
+  
 function updateValues()
 {
-displayShipHealth.innerHTML= "Ship Health:  "+shipHealth;
+displayShipHealthElement.innerHTML= "Ship Health:  "+shipHealth;
 
-document.getElementById("totalEnergy").innerHTML= "Total Energy to be consumed:  "+getTotalEnergyUse();
+//if scores change, update their new values in the document
+document.getElementById("enginesRating").innerHTML=enginesScore;
+document.getElementById("weaponsRating").innerHTML=weaponsScore;
+document.getElementById("shieldsRating").innerHTML=shieldsScore;
+document.getElementById("sensorsRating").innerHTML=sensorsScore;
+
+//update the modified system values
+enginesModifiedElement.innerHTML=enginesScore*(enginesPercent/100);
+weaponsModifiedElement.innerHTML=weaponsScore*(weaponsPercent/100);
+shieldsModifiedElement.innerHTML=shieldsScore*(shieldsPercent/100);
+sensorsModifiedElement.innerHTML=sensorsScore*(sensorsPercent/100);
+
+
+var energyDisplayArea = document.getElementById("totalEnergy").innerHTML= "Total Energy to be consumed:  "+getTotalEnergyUse();
 energyDisplayArea.innerHTML="Available Energy: "+energy+" / "+maxEnergy;
 }
 
 function getTotalEnergyUse()
 {
 var total=0;
-total+=parseInt(document.getElementById("enginesDisplayValue").innerHTML);
-total+=parseInt(document.getElementById("weaponsDisplayValue").innerHTML);
-total+=parseInt(document.getElementById("shieldsDisplayValue").innerHTML);
-total+=parseInt(document.getElementById("sensorsDisplayValue").innerHTML);
+total+=parseInt(enginesModifiedElement.innerHTML);
+total+=parseInt(weaponsModifiedElement.innerHTML);
+total+=parseInt(shieldsModifiedElement.innerHTML);
+total+=parseInt(sensorsModifiedElement.innerHTML);
+
+
+
 return total;
 }
 
@@ -166,6 +201,51 @@ function getDistance(x1,y1,x2,y2)
 var b = y1 - y2;
 
 var c = Math.sqrt( a*a + b*b );
-
+c/=3
 return c;
+}
+
+function travel()
+{
+
+var distance = getDistance(ship.x,ship.y,selectedLocation.x,selectedLocation.y);
+var energyConsumption = Math.round(distance*1000);
+	if (distance>0)
+	{
+		var answer=confirm("Are you sure you want to set a course for "+selectedLocation.name+"? \n This trip will take "+energyConsumption+" units of your energy reserves.");
+		if (energy >= energyConsumption)
+		{
+			if (answer==true)
+			{
+				ship.x=selectedLocation.x;
+				ship.y=selectedLocation.y;
+	
+			}
+		
+		}
+		else {alert("You do not have enough energy to make this trip.");}
+		
+	}else {alert("You are already at that destination");}
+	
+
+redrawCanvas();
+}
+
+
+
+function redrawCanvas()
+{
+	context.clearRect(0,0, canvas.width, canvas.height);
+	context.strokeStyle="yellow";
+context.strokeRect(selectedLocation.x-10, selectedLocation.y-10, 20, 20);
+context.font = "12px Arial";
+context.fillStyle="white";
+context.fillText("You are here",ship.x+10, ship.y);
+var i=0;
+for (i=0; i<all_locations.length; i+=1)
+{
+	drawCircle(all_locations[i].x, all_locations[i].y, 10, all_locations[i].color);
+
+}
+
 }
